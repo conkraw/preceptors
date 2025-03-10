a73501be905a10aff7668dcbcbbe7c2db0bd4276
conkraw <conkraw@gmail.com>
2025-03-10 14:09:22 -0400

    Create preceptorapp.py

---
+++ preceptorapp.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import io
+import re
+import pytz
+import firebase_admin
+from firebase_admin import credentials, firestore
+import openai
+
+########################################
+# 1) OPENAI & FIREBASE SETUP
+########################################
+
+openai.api_key = st.secrets["openai"]["api_key"]
+
+firebase_creds = st.secrets["firebase_service_account"].to_dict()
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_creds)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
