cd46d7c2b9a1f5d32cdf26a27215721eea840dd6
conkraw <conkraw@gmail.com>
2025-03-10 14:16:03 -0400

    Update preceptorapp.py

--- preceptorapp.py
+++ preceptorapp.py
@@ -34,9 +34,8 @@ with col1:
     st.subheader("Setup Analysis Report")
     # Embed the website so users can view it directly
     components.iframe(
-        "https://oasis.hersheymed.net/admin/course/e_manage/faculty/setup_analysis_report.html",
-        height=600,
-    )
+    "https://oasis.hersheymed.net/admin/course/e_manage/faculty/setup_analysis_report.html",
+    height=600)
     # File uploader for the analysis report (adjust allowed types as needed)
     analysis_report_file = st.file_uploader("Upload Analysis Report", type=["pdf", "docx", "csv"])
 
