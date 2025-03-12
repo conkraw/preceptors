35275cdf16100a85ab5fda582ab8eb1928c09cf4
conkraw <conkraw@gmail.com>
2025-03-12 10:00:08 -0400

    Update preceptorapp.py

--- preceptorapp.py
+++ preceptorapp.py
@@ -608,11 +608,11 @@ if analysis_report_file is not None:
                 details_table.cell(1, 1).text = str(row['Rotation Period'])
 
                 # Row 2: Number of Evaluations
-                details_table.cell(2, 0).text = "Preceptor Evaluations Completed By Student:"
+                details_table.cell(2, 0).text = "Student-Completed Preceptor Evaluations (n):"
                 details_table.cell(2, 1).text = str(row['num_evaluations'])
                 
                 # Row 3: Number of Student Evaluations Completed by Evaluator
-                details_table.cell(3, 0).text = "Student Evaluations Completed By Preceptor:"
+                details_table.cell(3, 0).text = "Preceptor-Completed Student Evalulations (n):"
                 details_table.cell(3, 1).text = str(row['total_evaluations'])
                 
                 # Row 4: Percentage of Student Evaluations Completed within 14 days
